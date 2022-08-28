7261b2b5a43238dd91904f17f244c27379ad8436
Hwang Jung Hyeon <1004wipi@gmail.com>
2022-08-28 16:30:12 +0900

    solve: [#4] 프로그래머스 오픈채팅방

---
+++ algorithm/js/오프채팅방.js
@@ -0,0 +1,20 @@
+function solution(record) {
+    let result = [];
+    const map = {};
+
+    for(let i= 0; i < record.length; i++)
+    {
+        const [state, uid, name] = record[i].split(' ');
+        
+        if (state === "Leave"){
+            result.push([uid, '님이 나갔습니다.']);
+            continue;
+        }
+        if (state === "Enter")
+            result.push([uid, '님이 들어왔습니다.'])
+            
+        map[uid] = name
+    }
+    
+    return result.map(x => map[x[0]] + x[1]);
+}
